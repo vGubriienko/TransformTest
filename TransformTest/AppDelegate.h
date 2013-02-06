7ce8f2131fbdc666d9201a60013de4d012058f3f
Viktor Gubriienko <viktor@micro-b.net>
2013-02-06 15:55:58 +0200

    Initial Commit

---
+++ TransformTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TransformTest
+//
+//  Created by Viktor Gubriienko on 06.02.13.
+//  Copyright (c) 2013 Viktor Gubriienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TransformTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TransformTest
+//
+//  Created by Viktor Gubriienko on 06.02.13.
+//  Copyright (c) 2013 Viktor Gubriienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
