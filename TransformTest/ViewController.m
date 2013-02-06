fd9aaaaff1c388f69be4ffe5594e73422c97caa5
Viktor Gubriienko <viktor@micro-b.net>
2013-02-06 17:06:43 +0200

    Initial commit

--- TransformTest/ViewController.m
+++ TransformTest/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+- (IBAction)clickImage:(UIButton *)sender;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttons;
+
 @end
 
 @implementation ViewController
@@ -17,7 +20,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[_buttons valueForKey:@"imageView"] setValue:@(UIViewContentModeScaleAspectFit) forKey:@"contentMode"];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +31,64 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)clickImage:(UIButton *)sender {
+
+    CGRect fromRect = sender.frame;
+    CGRect toRect = CGRectMake(10.0f, 10.0f, 300.0f, 200.0f);
+    
+    UIImageView *testImageView = [[UIImageView alloc] initWithImage:[sender imageForState:UIControlStateNormal]];
+    testImageView.backgroundColor = [UIColor brownColor];
+    testImageView.contentMode = UIViewContentModeScaleAspectFit;
+    testImageView.tag = 100;
+    
+    CGAffineTransform transfrom = [self translatedAndScaledTransformUsingViewRect:toRect fromRect:fromRect];
+    
+    testImageView.frame = fromRect;
+    [self.view addSubview:testImageView];
+    
+    [UIView animateWithDuration:2 animations:^{
+        testImageView.transform = transfrom;
+    }];
+    
+    
+    
+    UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+    [self.view addGestureRecognizer:recognizer];
+    [_buttons setValue:@NO forKey:@"enabled"];
+}
+
+- (void)handleTap:(UITapGestureRecognizer *)sender {
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        UIView *testView = [self.view viewWithTag:100];
+        [testView removeFromSuperview];
+        [self.view removeGestureRecognizer:sender];
+        [_buttons setValue:@YES forKey:@"enabled"];
+    }
+}
+
+- (CGAffineTransform)translatedAndScaledTransformUsingViewRect:(CGRect)viewRect fromRect:(CGRect)fromRect {
+    float xtransl = viewRect.origin.x - fromRect.origin.x;
+    float ytransl = viewRect.origin.y - fromRect.origin.y;
+    //float xScale = viewRect.size.width / fromRect.size.width;
+    //float yScale = viewRect.size.height / fromRect.size.height;
+    
+    NSLog(@"xtransl %f, ytransl %f", xtransl, ytransl);
+    
+    
+    CGAffineTransform rectScale = CGAffineTransformMakeScale(viewRect.size.width / fromRect.size.width,
+                                                             viewRect.size.height / fromRect.size.height);
+    
+    CGPoint orig = CGPointMake(fromRect.origin.x, fromRect.origin.y);
+    CGPoint newOrig = CGPointApplyAffineTransform(orig, rectScale);
+    
+    
+    return CGAffineTransformMake(viewRect.size.width / fromRect.size.width,
+                                 0,
+                                 0,
+                                 viewRect.size.height / fromRect.size.height,
+                                 viewRect.origin.x - newOrig.x,
+                                 viewRect.origin.y - newOrig.y);
+}
+
+    
 @end
